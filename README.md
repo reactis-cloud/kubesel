58f42088db095ca139586eb484822e036f83ec92
Ondrej Novak <novakondrej@seznam.cz>
2019-11-07 21:44:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Kubernetes Cluster Select
+
+Just copy predefined config file to ~/.kube/config
+![Alt text](kubesel.gif?raw=true "")
+
+### Install
+```
+git clone https://github.com/reactis-cloud/kubesel.git
+cd kubesel
+npm install
+chmod +x ./kubesel.js
+ln -s "$(pwd)"/kubesel.js /usr/local/bin/ks
+```
+
+### Config example 
+~/.kubesel/config.yaml
+```
+clusters:
+  - name: Hetzner
+    config: ~/.kube/master
+    color: 91
+  - name: Backup
+    config: ~/.kube/backup
+    color: 96
+  - name: Digital Ocean
+    config: ~/.kube/do
+    color: 96
+  - name: Development
+    config: ~/.kube/dev
+```
+Bash colors: https://misc.flogisoft.com/bash/tip_colors_and_formatting 
+
+### Configure bash prompt
+ ~/.bashrc
+```
+kubesel_prompt()
+{
+ PS1="\\u@\\h[\e[`cat ~/.kubesel/color.selected`m`cat ~/.kubesel/name.selected`\e[39m]:\\w\\$ "
+}
+PROMPT_COMMAND=kubesel_prompt
+
+```
---
+++ kubesel.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const yaml = require('yaml');
+const fs = require('fs');
+const prompts = require('prompts');
+const expandTilde = require('expand-tilde');
+const homedir = require('os').homedir();
+const kubedir = homedir + '/.kube';
+const kubeseldir = homedir + '/.kubesel';
+const file = fs.readFileSync(kubeseldir + '/config.yaml', 'utf8')
+const config = yaml.parse(file);
+
+(async () => {
+	const response = await prompts({
+		type: 'select',
+		name: 'value',
+		message: 'Select a kubernetes cluster',
+		choices: config.clusters.map((item) => { return  {title: item.name, value: item}}),
+		initial: 0
+	});
+	if (response.value) {
+		fs.copyFile(expandTilde(response.value.config), kubedir + '/config', (err) => {
+			if (err) throw err;
+			else {
+				fs.writeFile(kubeseldir + '/name.selected', response.value.name, () => {
+
+				});
+				fs.writeFile(kubeseldir + '/color.selected', response.value.color ? response.value.color : '', () => {
+
+				});
+			}
+		});
+	}
+})();
